3ffbf64f44722c031851d1d1f3d180efa7f6812a
mihaivo <mihai1voicescu@gmail.com>
2019-12-20 14:41:49 +0200

    Add extractors

---
+++ lib/extractors.dart
@@ -0,0 +1,18 @@
+import 'package:charts_flutter/flutter.dart' as charts;
+
+import 'model.dart';
+
+class Extractor {
+  static extract(List<StockPoint> list) {
+    return [list.map((e) => (e.close)), list.map((e) => e.timestamp)];
+  }
+
+  static chartTimeSeries(List<StockPoint> list) {
+    return charts.Series<StockPoint, DateTime>(
+      id: 'Sales',
+      domainFn: (StockPoint sales, _) => sales.timestamp,
+      measureFn: (StockPoint sales, _) => sales.close,
+      data: list,
+    );
+  }
+}
