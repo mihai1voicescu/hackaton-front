6a5b514f9245c6bf39718a46dcfd8421062c3360
mihaivo <mihai1voicescu@gmail.com>
2019-12-20 11:48:05 +0200

    Add stockpoint

--- lib/model.dart
+++ lib/model.dart
@@ -9,5 +9,20 @@ class User {
   User({this.firstName, this.name});
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
   Map<String, dynamic> toJson() => _$UserToJson(this);
+}
+
+@JsonSerializable(nullable: false)
+class StockPoint {
+  final int value;
+  final DateTime time;
+  final String name, sxName;
+
+  StockPoint(this.name, this.value, this.time, {this.sxName = "Default"});
+
+  factory StockPoint.fromJson(Map<String, dynamic> json) =>
+      _$StockPointFromJson(json);
+
+  Map<String, dynamic> toJson() => _$StockPointToJson(this);
 }
--- lib/model.g.dart
+++ lib/model.g.dart
@@ -17,3 +17,20 @@ Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'name': instance.name,
       'firstName': instance.firstName,
     };
+
+StockPoint _$StockPointFromJson(Map<String, dynamic> json) {
+  return StockPoint(
+    json['name'] as String,
+    json['value'] as int,
+    DateTime.parse(json['time'] as String),
+    sxName: json['sxName'] as String,
+  );
+}
+
+Map<String, dynamic> _$StockPointToJson(StockPoint instance) =>
+    <String, dynamic>{
+      'value': instance.value,
+      'time': instance.time.toIso8601String(),
+      'name': instance.name,
+      'sxName': instance.sxName,
+    };
